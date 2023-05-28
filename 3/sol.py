ab0eb4d653d357a86ed0e493bea3e1ad77d8b31f
Shubham Saini <ncitshubham@gmail.com>
2023-05-28 22:54:10 +0530

    complete day 3 part 2

--- 3/sol.py
+++ 3/sol.py
@@ -5,7 +5,7 @@ char_priority = {
 }
 
 
-def cal_total_priority():
+def cal_total_priority_p1():
     with open("input.txt") as inp:
         rucksacks = inp.read().splitlines()
         total_priority = 0
@@ -23,8 +23,27 @@ def cal_total_priority():
         return total_priority
 
 
+def cal_total_priority_p2():
+    with open("input.txt") as inp:
+        rucksacks = inp.read().splitlines()
+        total_priority = 0
+        ELF_GROUP_SIZE = 3
+
+        for idx in range(0, len(rucksacks), ELF_GROUP_SIZE):
+            items_in_first_rucksack = set(rucksacks[idx])
+            items_in_second_rucksack = set(rucksacks[idx+1])
+            items_in_third_rucksack = set(rucksacks[idx+2])
+
+            common_item = (items_in_first_rucksack &
+                           items_in_second_rucksack & items_in_third_rucksack).pop()
+
+            total_priority += char_priority[common_item]
+
+        return total_priority
+
+
 def main():
-    print(cal_total_priority())
+    print(cal_total_priority_p2())
 
 
 if __name__ == "__main__":
