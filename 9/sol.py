57b8b58a29a809ba8b1f698186ae7f380bd3ec51
Shubham Saini <ncitshubham@gmail.com>
2023-07-17 00:15:51 +0530

    complete day 9 part 2

--- 9/sol.py
+++ 9/sol.py
@@ -1,21 +1,20 @@
-import pprint
-
-
 class Solution:
-    def __init__(self, input_file):
+    def __init__(self, input_file, no_of_knots):
         self.input_file = input_file
         matrix_size = 500
-        initial_pos = matrix_size//2  # place the head and tail in the middle initially
+        initial_pos = matrix_size//2  # place the head in the middle initially
 
         # Create a zero-filled matrix of size matrix size
         self.matrix = [[0 for _ in range(matrix_size)]
                        for _ in range(matrix_size)]
 
-        # initialize the position of head and tail
-        self.head_row = self.head_col = self.tail_row = self.tail_col = initial_pos
+        # initialize the position of head and all knots in a list
+        # for every position, we are storing the row and col position
+        self.positions = [[initial_pos, initial_pos]
+                          for _ in range(no_of_knots + 1)]
 
-        #  mark the position as visited
-        self.matrix[self.head_row][self.head_col] = 1
+        # mark the position as visited
+        self.matrix[initial_pos][initial_pos] = 1
 
     def move_head(self):
         with open(self.input_file) as file:
@@ -26,45 +25,53 @@ class Solution:
                 steps = int(steps)
 
                 for _ in range(steps):
+
+                    # move the head which is at self.positions[0]
                     if direction == "R":
-                        self.head_col += 1
+                        self.positions[0][1] += 1
                     elif direction == "L":
-                        self.head_col -= 1
+                        self.positions[0][1] -= 1
                     elif direction == "U":
-                        self.head_row -= 1
+                        self.positions[0][0] -= 1
                     else:
-                        self.head_row += 1
+                        self.positions[0][0] += 1
 
-                    row_diff = self.head_row - self.tail_row
-                    col_diff = self.head_col - self.tail_col
+                    # loop through all knots and update their positions
+                    for knot_no in range(1, len(self.positions)):
+                        curr_knot = self.positions[knot_no]
+                        prev_knot = self.positions[knot_no - 1]
 
-                    # if distance is more than 1, move tail
-                    if abs(row_diff) > 1 or abs(col_diff) > 1:
-                        # move tail colwise if row is same
-                        if self.head_row == self.tail_row:
-                            if self.head_col > self.tail_col:
-                                self.tail_col += 1
+                        row_diff = prev_knot[0] - curr_knot[0]
+                        col_diff = prev_knot[1] - curr_knot[1]
+
+                        # if distance is more than 1, move tail
+                        if abs(row_diff) > 1 or abs(col_diff) > 1:
+                            # move tail colwise if row is same
+                            if prev_knot[0] == curr_knot[0]:
+                                if prev_knot[1] > curr_knot[1]:
+                                    curr_knot[1] += 1
+                                else:
+                                    curr_knot[1] -= 1
+                            # move tail rowwise if col is same
+                            elif prev_knot[1] == curr_knot[1]:
+                                if prev_knot[0] > curr_knot[0]:
+                                    curr_knot[0] += 1
+                                else:
+                                    curr_knot[0] -= 1
+                            # move tail diagnolly if both row and col are different
                             else:
-                                self.tail_col -= 1
-                        # move tail rowwise if col is same
-                        elif self.head_col == self.tail_col:
-                            if self.head_row > self.tail_row:
-                                self.tail_row += 1
-                            else:
-                                self.tail_row -= 1
-                        # move tail diagnolly if both row and col are different
-                        else:
-                            if row_diff > 0:
-                                self.tail_row += 1
-                            else:
-                                self.tail_row -= 1
-                            if col_diff > 0:
-                                self.tail_col += 1
-                            else:
-                                self.tail_col -= 1
+                                if row_diff > 0:
+                                    curr_knot[0] += 1
+                                else:
+                                    curr_knot[0] -= 1
+                                if col_diff > 0:
+                                    curr_knot[1] += 1
+                                else:
+                                    curr_knot[1] -= 1
 
                     # mark the tail position as visited
-                    self.matrix[self.tail_row][self.tail_col] = 1
+                    self.matrix[self.positions[-1][0]
+                                ][self.positions[-1][1]] = 1
 
     def count_visited(self):
         count = 0
@@ -76,7 +83,7 @@ class Solution:
 
 
 def main():
-    sol = Solution("9/input.txt")
+    sol = Solution("9/input.txt", 9)
     sol.move_head()
     print("visited:", sol.count_visited())
 
